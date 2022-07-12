9aeab4eadcdfdb89bad93dd4e6dc11b10f8f9fcb
Vlad Kravets <hazardvlad19@gmail.com>
2022-07-12 19:56:35 +0300

    made changes related to the cards

---
+++ src/features/cards/PacksList/PacksFilters/PacksFilters.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { packsActions } from '../../../../main/bll/reducers/packsReducer'
+import { useAppDispatch, useAppSelector } from '../../../../main/bll/store'
+import SuperButton from '../../../../main/ui/common/SuperButton/SuperButton'
+import { CardsCountRange } from './CardsCountRange/CardsCountRange'
+import s from './PacksFilters.module.css'
+import { PacksOwnerFilter } from './PacksOwnerFilter/PacksOwnerFilter'
+import { PacksSearchInput } from './PacksSearchInput/PacksSearchInput'
+import { SearchInfo } from './SearchInfo/SearchInfo'
+
+export const PacksFilters = () => {
+
+
+
+
+	return (
+		<div className={s.packBlock}>
+			<div className={s.filtersHeader}>
+				<h2 className={s.blockTitle}>Show packs</h2>
+				<SearchInfo />
+			</div>
+			<PacksSearchInput />
+			<PacksOwnerFilter />
+			<CardsCountRange />
+		</div>
+	)
+}
